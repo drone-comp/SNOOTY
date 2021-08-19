bdd2c2b5c63b7bbdc034003d66653939fafe45d9
Yuri Gama <63912211+yuri-gama@users.noreply.github.com>
2021-08-19 18:20:05 -0300

    draft for evaluate function

--- src/SNOOTY/__init__.py
+++ src/SNOOTY/__init__.py
@@ -68,3 +68,88 @@ def smooth(data, smooth_size, before, after):
     datapandas = r_pkg.smooth(data, smooth_size, before, after)
 
     return datapandas
+
+def mean_squared_error(pd_series, reference_value):
+    ''' 
+    This function calculates the mean squared error of the pandas.Series
+    data 'pd_series' and returns a float as result. The calculation is 
+    sum((pd_series[i] - reference_value)^2)/pd_series.size. If 'pd_series' is
+    empty, this function returns None. The type of the elements of pd_series must be
+    int or float. If pd_series is empty, this function returns None.
+    
+    Input:
+        pd_series       --> pd.Series
+        reference_value --> float
+    
+    Output:
+        result --> float
+    '''
+    error_result = {'pd_series':False, 'reference_value':False}
+    #Check if pd_series has type pd.Series:
+    if not isinstance(pd_series, (pd.core.series.Series)):
+        error_result['pd_series'] = True
+
+    #Check if reference_value is float or integer:
+    if not (isinstance(reference_value, (float, int))):
+        error_result['reference_value'] = True
+
+    #Raise exception if necessary:
+    if all(error_result.values()):
+        raise TypeError("'pd_series' must be a pandas.Series, and reference_value must be a float or an int")
+    elif error_result['pd_series']:
+        raise TypeError("'pd_series' must be a pandas.Series")
+    elif error_result['reference_value']:
+        raise TypeError("reference_value must be a float or an int")
+
+    #Check if pd_series is empty:
+    if pd_series.size == 0:
+        return None
+
+    #Calculate the mean squared error:
+    error         = pd_series - reference_value
+    squared_error = pow(error, 2)
+    mean_squared_error_value = sum(squared_error)/pd_series.size
+
+    #Return the result:
+    return mean_squared_error_value
+
+def mean(row, rolling_array, window_in, *kwargs):
+    row['window_{}_mean'.format(window_in)] = rolling_array.mean()
+
+def median(row, rolling_array, window_in, *args):
+    row['window_{}_median'.format(window_in)] = rolling_array.quantile(0.5)
+
+def first_quartile(row, rolling_array, window_in, *args):
+    row['window_{}_1rst_quartile'.format(window_in)] = rolling_array.quantile(0.25)
+
+def third_quartile(row, rolling_array, window_in, *args):
+    row['window_{}_3rd_quartile'.format(window_in)] = rolling_array.quantile(0.75)
+
+def max_function(row, rolling_array, window_in, *args):
+    row['window_{}_max'.format(window_in)]  = rolling_array.max()
+
+def min_function(row, rolling_array, window_in, *args):
+    row['window_{}_min'.format(window_in)]  = rolling_array.min()
+
+def mse_value(row, rolling_array, window_in, mean_squared_error_value):
+    row['mean_squared_error'] = mean_squared_error_value
+
+def evaluate(pd_series, reference_value, window_in, evaluate_op):
+
+    mean_squared_error_value = mean_squared_error(pd_series, reference_value) #np_array, reference_value)
+    rolling_array = pd_series.rolling(window_in).apply(mean_squared_error, raw=False, args=(reference_value,))
+
+    row = {}
+    functions_dic = {
+        'mean': mean,
+        '1q'  : first_quartile,
+        '3q'  : third_quartile,
+        'max' : max_function,
+        'min' : min_function,
+        'mse' : mse_value
+    }
+
+    for op in evaluate_op:
+        functions_dic[op](row, rolling_array, window_in, mean_squared_error_value)
+
+    return row
