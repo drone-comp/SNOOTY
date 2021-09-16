c6a5839b40d503db593f592cfd689dd95fc1c393
Filipe Verri <filipeverri@gmail.com>
2021-09-16 15:47:13 -0300

    Bumping version.

--- src/SNOOTY/__init__.py
+++ src/SNOOTY/__init__.py
@@ -9,15 +9,16 @@ from rpy2.robjects.packages import STAP
 import rpy2.robjects as ro
 import pandas as pd
 
+
 def smooth(data, smooth_size, before, after):
     """
-    
+
     Each column is used to calculate a smooth value using the slide_index_dbl from R language. After that it's made a time dependent linear interpolation applied to all columns
-    
+
     Parameters
     ----------
     data --> dataframe which needs a time column and at least another column to be smoothed.
-    
+
 
     before, afeter --> represents the number of values before or after the current cell to include in the sliding window.
 
@@ -44,7 +45,7 @@ def smooth(data, smooth_size, before, after):
             ndata <- ndata %>% mutate( "{name}" := approx(data$time, smooth, ntime)$y)
         }
 
-     
+
         colnames(ndata) <- c("time", names)
 
         return(ndata)
@@ -58,70 +59,80 @@ def smooth(data, smooth_size, before, after):
 
     return datapandas
 
+
 def mean_squared_error(pd_series, reference_value):
-    ''' 
+    '''
     This function calculates the mean squared error of the pandas.Series
-    data 'pd_series' and returns a float as result. The calculation is 
+    data 'pd_series' and returns a float as result. The calculation is
     sum((pd_series[i] - reference_value)^2)/pd_series.size. If 'pd_series' is
     empty, this function returns None. The type of the elements of pd_series must be
     int or float. If pd_series is empty, this function returns None.
-    
+
     Input:
         pd_series       --> pd.Series
         reference_value --> float
-    
+
     Output:
         result --> float
     '''
-    error_result = {'pd_series':False, 'reference_value':False}
-    #Check if pd_series has type pd.Series:
+    error_result = {'pd_series': False, 'reference_value': False}
+    # Check if pd_series has type pd.Series:
     if not isinstance(pd_series, (pd.core.series.Series)):
         error_result['pd_series'] = True
 
-    #Check if reference_value is float or integer:
+    # Check if reference_value is float or integer:
     if not (isinstance(reference_value, (float, int))):
         error_result['reference_value'] = True
 
-    #Raise exception if necessary:
+    # Raise exception if necessary:
     if all(error_result.values()):
-        raise TypeError("'pd_series' must be a pandas.Series, and reference_value must be a float or an int")
+        raise TypeError(
+            "'pd_series' must be a pandas.Series, and reference_value must be a float or an int")
     elif error_result['pd_series']:
         raise TypeError("'pd_series' must be a pandas.Series")
     elif error_result['reference_value']:
         raise TypeError("reference_value must be a float or an int")
 
-    #Check if pd_series is empty:
+    # Check if pd_series is empty:
     if pd_series.size == 0:
         return None
 
-    #Calculate the mean squared error:
-    error         = pd_series - reference_value
+    # Calculate the mean squared error:
+    error = pd_series - reference_value
     squared_error = pow(error, 2)
-    mean_squared_error_value = sum(squared_error)/pd_series.size
+    mean_squared_error_value = sum(squared_error) / pd_series.size
 
-    #Return the result:
+    # Return the result:
     return mean_squared_error_value
+
 
 def mean(row, rolling_array, window_in, *kwargs):
     row['mean'.format(window_in)] = rolling_array.mean()
 
+
 def median(row, rolling_array, window_in, *args):
     row['median'.format(window_in)] = rolling_array.quantile(0.5)
+
 
 def first_quartile(row, rolling_array, window_in, *args):
     row['1rst_quartile'.format(window_in)] = rolling_array.quantile(0.25)
 
+
 def third_quartile(row, rolling_array, window_in, *args):
     row['3rd_quartile'.format(window_in)] = rolling_array.quantile(0.75)
 
+
 def max_function(row, rolling_array, window_in, *args):
-    row['max'.format(window_in)]  = rolling_array.max()
+    row['max'.format(window_in)] = rolling_array.max()
+
 
 def min_function(row, rolling_array, window_in, *args):
-    row['min'.format(window_in)]  = rolling_array.min()
+    row['min'.format(window_in)] = rolling_array.min()
+
 
 def mse_value(row, rolling_array, window_in, mean_squared_error_value):
     row['mean_squared_error'] = mean_squared_error_value
+
 
 def evaluate(pd_series, reference_value, window_in, evaluate_op):
     """
@@ -135,29 +146,33 @@ def evaluate(pd_series, reference_value, window_in, evaluate_op):
         window_in = windowing float size
 
         evaluate_op = list that selects which operation should be returned
-                possible values: 
+                possible values:
                                 mean, 1q, 3q, max, min, mse
                                 1q = first quartile
                                 3q = third quartile
                                 mse = mean square error
     """
 
-
-
-    mean_squared_error_value = mean_squared_error(pd_series, reference_value) #np_array, reference_value)
-    rolling_array = pd_series.rolling(window_in).apply(mean_squared_error, raw=False, args=(reference_value,))
+    mean_squared_error_value = mean_squared_error(
+        pd_series, reference_value)  # np_array, reference_value)
+    rolling_array = pd_series.rolling(window_in).apply(
+        mean_squared_error, raw=False, args=(reference_value,))
 
     row = {}
     functions_dic = {
         'mean': mean,
-        '1q'  : first_quartile,
-        '3q'  : third_quartile,
-        'max' : max_function,
-        'min' : min_function,
-        'mse' : mse_value
+        '1q': first_quartile,
+        '3q': third_quartile,
+        'max': max_function,
+        'min': min_function,
+        'mse': mse_value
     }
 
     for op in evaluate_op:
-        functions_dic[op](row, rolling_array, window_in, mean_squared_error_value)
+        functions_dic[op](
+            row,
+            rolling_array,
+            window_in,
+            mean_squared_error_value)
 
     return row
